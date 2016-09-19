44c30040633f34bd2eab593e0ca879220eb7783e
admirkb <kelvin@admir.tv>
2016-09-19 17:03:47 +0100

    Update user.service.ts

--- app/shared/user.service.ts
+++ app/shared/user.service.ts
@@ -24,4 +24,4 @@ export class UserService {
 Copyright 2016 Google Inc. All Rights Reserved.
 Use of this source code is governed by an MIT-style license that
 can be found in the LICENSE file at http://angular.io/license
-*/
+*/export class BillingComponent extends WebApiComponent implements OnInit {
